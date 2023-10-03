07c5a08d0b9411e4b9aaa6df53c355b88264ee16
dilhrm <121042602+dilhrm@users.noreply.github.com>
2023-10-03 12:35:38 -0400

    Add files via upload

---
+++ Truck.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Truck {
+
+    int truckID;
+    int maxWeight;
+    int height;
+    int length;
+    int width;
+    ArrayList<Box> boxes = new ArrayList<>();
+
+
+    void addBox(Box box){
+
+    }
+    void removeBox(int boxID){
+
+    }
+    int getLoadedWeight(){
+        int weight = 0;
+
+
+        return(weight);
+    }
+
+
+}
