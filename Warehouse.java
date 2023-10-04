9248f18b96a05b2eb2884f975b6fb7a7eb6fed1b
Razor177 <134652798+Razor177@users.noreply.github.com>
2023-10-04 12:30:32 -0400

    Add files via upload

--- Warehouse.java
+++ Warehouse.java
@@ -3,19 +3,25 @@ import java.io.File;
 
 public class Warehouse {
     int WarehouseID;
-    //Arryalist<Box> inventory;
-    //Arryalist<Truck> trucks;
-    // final int MAX_BOXES;
-    // final int MAX_TRUCKS;
+    ArrayList<Box> inventory = new ArrayList<>();
+    ArrayList<Truck> trucks = new ArrayList<>();
+    final int MAX_BOXES;
+    final int MAX_TRUCKS;
 
-    // public void addBox(Box box) {}
+    Warehouse(int id, int maxBoxes, int maxTrucks) {
+        this.WarehouseID = id;
+        this.MAX_BOXES = maxBoxes;
+        this.MAX_TRUCKS = maxTrucks;
+    }
+
+    public void addBox(Box box) {}
     public void removeBox(int boxID) {};
-    //public void addTruck(Truck truck) {}
+    public void addTruck(Truck truck) {}
     public void removeTruck(int truckID) {}
     public void uploadInventory(File file) {}
     public void downloadInventory(File file) {}
     //public void draw(Graphics g) {}
-    public String toString() {return}
+    //public String toString() {return}
 
 
 
